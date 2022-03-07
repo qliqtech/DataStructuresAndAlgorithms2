3109bcfcf4576b66ed1fbedab4df651cc8cb28fc
David Kumi <david.kumi@inpathgroup.africa>
2022-03-07 12:01:32 +0000

    Submit request structure

---
+++ NSS/FLAIR_WEB_SERVICES/FLAIR_USER_MANAGEMENT_AUTH_SERVICE/app/Http/Controllers/UserManagement.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserManagement extends Controller
+{
+
+    public function edituser(Request $request){
+
+
+
+
+
+    }
+
+
+}
