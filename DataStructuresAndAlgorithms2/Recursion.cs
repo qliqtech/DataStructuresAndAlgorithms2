98c4f57a26a301f1cd235e495f643e6046d93cd3
David Kumi <david.kumi@inpathgroup.africa>
2022-05-23 09:06:15 +0000

    my commit

---
+++ DataStructuresAndAlgorithms2/Recursion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms2
+{
+    public class Recursion
+    {
+
+
+        public void calculateiterative(int n) {
+
+            while (n > 0) {
+
+                int k = n * n;
+                Console.WriteLine(k);
+                n = n - 1;
+
+            
+            }
+
+        
+        }
+
+        public void calculaterecursive(int n)
+        {
+
+            if (n > 0)
+            {
+
+                int k = n * n;
+                Console.WriteLine(k);
+                //   n = n - 1;
+
+                calculaterecursive(n - 1);
+            }
+
+
+        }
+
+
+
+    }
+}
